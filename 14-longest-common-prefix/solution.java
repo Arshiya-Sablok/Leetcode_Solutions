74d67416ae4f3a7c999db5b6cf6d1520e1432930
Arshiya Sablok <arshiyaofficial712@gmail.com>
2026-09-15 20:58:03 +0530

    Longest Common Prefix - 2026-09-15T15:28:01.486Z

---
+++ 14-longest-common-prefix/solution.java
@@ -0,0 +1,22 @@
+// 0 ms | 43.3 MB
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.equals("")) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
+
